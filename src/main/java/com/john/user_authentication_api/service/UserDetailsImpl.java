4140c67256d6d08e9a7f7b67fe991bcfa194080f
Jonathan Albuquerque <jonathanalbuquerquq@gmail.com>
2026-02-18 15:28:23 -0300

    cria estrutura inicial do módulo de segurança JWT

---
+++ src/main/java/com/john/user_authentication_api/service/UserDetailsImpl.java
@@ -0,0 +1,33 @@
+package com.john.user_authentication_api.service;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+}
---
+++ src/main/java/security/jwt/JwtUtils.java
@@ -0,0 +1,35 @@
+package security.jwt;
+
+import com.john.user_authentication_api.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${projeto.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${projeto.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateToken(UserDetailsImpl userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(getSingninKey(), SignatureAlgorithm.HS512).compact();
+    }
+
+    public Key getSingninKey(){
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+        return key;
+    }
+}
